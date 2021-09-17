fa4d6d582933c50b4a27fce6017d96c674cd6226
Gvein <kern1702@yandex.ru>
2021-09-17 10:19:15 +0300

    Update Keras_graphconv.py

--- Source/Keras_graphconv.py
+++ Source/Keras_graphconv.py
@@ -159,7 +159,6 @@ class GraphConvModelMod(KerasModel):
             if self.pos_weight:
                 loss = WeightedSigmoidCrossEntropy(self.pos_weight)
             else:
-                # loss = MacroSoftF1()
                 loss = SoftmaxCrossEntropy()
         else:
             output = Dense(n_tasks, trainable=False)(self.neural_fingerprint)
@@ -228,39 +227,6 @@ class GraphConvModelMod(KerasModel):
                     inputs.append(multiConvMol.get_deg_adjacency_lists()[i])
                 yield (inputs, [y_b], [w_b])
 
-
-def macro_soft_f1(y, y_hat):
-    """Compute the macro soft F1-score as a cost.
-    Average (1 - soft-F1) across all labels.
-    Use probability values instead of binary predictions.
-
-    Args:
-        y (int32 Tensor): targets array of shape (BATCH_SIZE, N_LABELS)
-        y_hat (float32 Tensor): probability matrix of shape (BATCH_SIZE, N_LABELS)
-
-    Returns:
-        cost (scalar Tensor): value of the cost function for the batch
-    """
-
-    y = tf.cast(y, tf.float32)
-    y_hat = tf.cast(y_hat, tf.float32)
-    tp = tf.reduce_sum(y_hat * y, axis=0)
-    fp = tf.reduce_sum(y_hat * (1 - y), axis=0)
-    fn = tf.reduce_sum((1 - y_hat) * y, axis=0)
-    soft_f1 = 2 * tp / (2 * tp + fn + fp + 1e-16)
-    cost = 1 - soft_f1  # reduce 1 - soft-f1 in order to increase soft-f1
-    macro_cost = tf.reduce_mean(cost)  # average on all labels
-
-    return macro_cost
-
-
-class MacroSoftF1(Loss):
-    """
-
-    """
-    def __call__(self, output, labels):
-        output, labels = _make_shapes_consistent(output, labels)
-        return macro_soft_f1(labels, output)
 
 
 class WeightedSigmoidCrossEntropy(Loss):
